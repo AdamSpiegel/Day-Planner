a6c3a475559d1a04ccfd0f4023c9c41ef8ff8563
Adam Spiegel <adamspiegel23@gmail.com>
2021-04-23 17:56:52 -0400

    update repository

--- README.md
+++ README.md
@@ -1,19 +1,31 @@
-# Day-Planner
+# Work Day Scheduler - Day Planner
 
 ## Project Description & Goals
+   * Our goal for this project was to develop out starter code, into a simple calander application which tracks the users' work day events (to be entered by the user).  The calander was to run from the standard 9:00am-5:00pm work day.  
+
+   * Our objective included upating the HTML starter code, along with modifying the CSS (powered by jQuerey in this application).  In order to work with date and time functionality (current day/time displayed at the top of the screen), the Moment.js library was to be utilized.  
    
+   * This library was to be utlized as well, so that the Day Planner would highlight in grey (past), present (red) and future (green) dependant upon what time of the day it currently is.
+
+   * Finally, upon clicking on a save function, the users' input was to be stored and logged (utilizing local storage).
 
 
 ## Action Items 
 
-    1 
-    2  
-    3
-    4
-    5
+    1) After initial review of the Work Day Scheduler starter code, I began by incorporating a JavaScript file by adding the script to the bottom of the HTML.
+    2) Next, the build out process began for the time blocks.  Utilizing Boot Strap for starter code assitance, I was able to incorporate all rows within the container.  All of which follow a similar pattern for every hour of the work day (9am-5pm)
+    3) Included within each row were paragraph and div class, both of which established the parameters for the rows (including size, a form control text area, in addition to a toggle primary button (which most closeley mimicked the demo on Bootstrap)). 
+    4) Following the creation of my time blocks, I proceeded to the Javascript.  I began from the beginning and utilizing the moment.js library, to perfrom a function pulling in the current day to the page's header.  This format was (moment().format('ddd, MMM Do YYYY')).
+    5) After the current day display was established at the top of the page, I proceeded to build out my variables to create an active Javascript.  These variables were created to be included in seperate Event Listeners, one for every button (9am-5pm), in order for the user to be able to successfully enter data and save into the text fields. These functions rely on document.GetElementById as well, and all console log correctly to display the users' input is being saved.
+    
+
+    **Items in Progress/Troubleshooting 
+    
 
 
- <!-- <img src="JS Code Quiz.png" alt="JS Code Quiz Image"> -->
+ <img src="Screenshot (34).png" alt="Day Planner Image 1">
+
+ <img src="Screenshot (35).png" alt="Day Planner Image 2"> 
 
 
 
---
+++ myScript1.js
@@ -0,0 +1,97 @@
+var currentDay = moment().format('ddd, MMM Do YYYY');
+$("#currentDay").html(currentDay);
+
+var workHours = ["9", "10", "11", "12", "1", "2", "3", "4", "5"];
+var container = $('#container');
+var textArea = $('.textarea');
+
+var textArea9 = document.getElementById("textarea9")
+var buttton9 = document.getElementById ("button9")
+button9.addEventListener("click", function (event){
+console.log(textArea9.value)
+})
+
+var textArea10 = document.getElementById("textarea10")
+var buttton10 = document.getElementById ("button10")
+button10.addEventListener("click", function (event){
+console.log(textArea10.value)
+})
+
+var textArea11 = document.getElementById("textarea11")
+var buttton11 = document.getElementById ("button11")
+button11.addEventListener("click", function (event){
+console.log(textArea11.value)
+})
+
+var textArea12 = document.getElementById("textarea12")
+var buttton12 = document.getElementById ("button12")
+button12.addEventListener("click", function (event){
+console.log(textArea12.value)
+})
+
+var textArea1 = document.getElementById("textarea1")
+var buttton1 = document.getElementById ("button1")
+button1.addEventListener("click", function (event){
+console.log(textArea1.value)
+})
+
+var textArea2 = document.getElementById("textarea2")
+var buttton2 = document.getElementById ("button2")
+button2.addEventListener("click", function (event){
+console.log(textArea2.value)
+})
+
+var textArea3 = document.getElementById("textarea3")
+var buttton3 = document.getElementById ("button3")
+button3.addEventListener("click", function (event){
+console.log(textArea3.value)
+})
+
+var textArea4 = document.getElementById("textarea4")
+var buttton4 = document.getElementById ("button4")
+button4.addEventListener("click", function (event){
+console.log(textArea4.value)
+})
+
+var textArea5 = document.getElementById("textarea5")
+var buttton5 = document.getElementById ("button5")
+button5.addEventListener("click", function (event){
+console.log(textArea5.value)
+})
+
+// var moment = require('moment');
+// require('moment/locale/cs');
+// console.log(moment.locale()); // cs
+
+// Get/SetItem used for localStorage?
+
+// Do I need to use setInterval function to format calander time progressing? Moment.js?
+
+
+// GIVEN I am using a daily planner to create a schedule
+
+// WHEN I open the planner
+// THEN the current day is displayed at the top of the calendar - DONE
+
+// WHEN I scroll down
+// THEN I am presented with timeblocks for standard business hours - DONE
+
+// WHEN I view the timeblocks for that day
+// THEN each timeblock is color coded to indicate whether it is in the past, present, or future - ???
+
+// WHEN I click into a timeblock
+// THEN I can enter an event
+// WHEN I click the save button for that timeblock -DONE
+
+
+// THEN the text for that event is saved in local storage  - ???
+// WHEN I refresh the page
+// THEN the saved events persist
+
+
+
+
+
+
+
+
